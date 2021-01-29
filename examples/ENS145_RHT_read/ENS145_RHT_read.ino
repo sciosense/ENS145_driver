fdf2b0e87bae6510d7c16e6d718574e0161cee76
Sciosense <38984838+sciosense@users.noreply.github.com>
2021-01-29 14:48:18 +0100

    Add availability check, correct RHT readout

--- examples/ENS145_RHT_read/ENS145_RHT_read.ino
+++ examples/ENS145_RHT_read/ENS145_RHT_read.ino
@@ -1,6 +1,7 @@
 /*
   Example code for the ENS145 Development Shield
-  including read-out of temperature & humidity sensor
+
+  This example will read the ENS145 as well as the ENS210
   
   2021 Jan 24   v0.3  Christoph Friese
   2021 Jan 21   v0.2  Patryk Hallek
@@ -8,20 +9,31 @@
 
 */
 
+// Library to drive the ENS145 sensor
 #include <ScioSense_ENS145.h>
 ScioSense_ENS145 ens145;
 
+// Library to drive the EN210 temperture & humidity sensor
 #include <ScioSense_ENS210.h>
 ScioSense_ENS210 ens210;
 
 void setup() {
   Serial.begin(9600);
   
-  while(!Serial);
+  while(!Serial) { delay(100); };
   
   Serial.println("ENS145 Development Shield demo");
   
+  Serial.print("ENS145...");
   ens145.begin(ENS145_ADC_RESOLUTION, false);       // debug mode enabled
+  // If ENS145 initialization is not successful, stop execution
+  if (!ens145.available()) {
+    Serial.println("failed!");
+    while (1) {
+      delay(500);   
+    }
+  } 
+  Serial.println("done!");
   
   // Set voltage of heaters for operation 
   uint16_t voltageP1 = 800;
@@ -35,39 +47,54 @@ void setup() {
   
   Serial.print("ENS210...");
   ens210.begin();
-  Serial.println(ens210.available() ? "done." : "failed!");
+  // If ENS210 initialization is not successful, then stop execution
+  if (!ens210.available()) {
+    Serial.println("failed!");
+    while (1) {
+      delay(500);   
+    }
+  } 
+  Serial.println("done.");
   ens210.setSingleMode(false);
 }
 
 void loop() {
 
-  Serial.print("Hotplate 1:");
-  Serial.print("\t");
-  Serial.print(ens145.measureHeaterVoltage(ENS145_HP1));
-  Serial.print("\t");
-  Serial.print(ens145.measureHeaterResistance(ENS145_HP1));     //expected below 100Ohm
-  Serial.print("\t");
-  Serial.print(ens145.measureSensorVoltage(ENS145_HP1));
-  Serial.print("\t");
-  Serial.print(ens145.measureSensorResistance(ENS145_HP1));     //expected up to 1MOhm
-  Serial.print("\t");
+  if (ens145.available()) {
+    Serial.print("ENS145 HP1:");
+    Serial.print("\t");
+    Serial.print(ens145.measureHeaterVoltage(ENS145_HP1));
+    Serial.print(" mV\t");
+    Serial.print(ens145.measureHeaterResistance(ENS145_HP1));     //expected below 100Ohm
+    Serial.print(" Ohm\t");
+    Serial.print(ens145.measureSensorVoltage(ENS145_HP1));
+    Serial.print(" mV\t");
+    Serial.print(ens145.measureSensorResistance(ENS145_HP1));     //expected up to 1MOhm
+    Serial.print(" Ohm\t");
   
-  Serial.print("Hotplate 3:");
-  Serial.print("\t");
-  Serial.print(ens145.measureHeaterVoltage(ENS145_HP3));
-  Serial.print("\t");
-  Serial.print(ens145.measureHeaterResistance(ENS145_HP3));     //expected below 100Ohm
-  Serial.print("\t");
-  Serial.print(ens145.measureSensorVoltage(ENS145_HP3));        //expected up to 1MOhm
-  Serial.print("\t");
-  Serial.print(ens145.measureSensorResistance(ENS145_HP3));
-  Serial.print("\t");
+    Serial.print("ENS145 HP3:");
+    Serial.print("\t");
+    Serial.print(ens145.measureHeaterVoltage(ENS145_HP3));
+    Serial.print(" mV\t");
+    Serial.print(ens145.measureHeaterResistance(ENS145_HP3));     //expected below 100Ohm
+    Serial.print(" Ohm\t");
+    Serial.print(ens145.measureSensorVoltage(ENS145_HP3));        //expected up to 1MOhm
+    Serial.print(" mV\t");
+    Serial.print(ens145.measureSensorResistance(ENS145_HP3));
+    Serial.print(" Ohm\t");
+  }
   
-  Serial.print("ENS210:");
-  Serial.print("\t");
-  Serial.print(ens210.getTempCelsius())
-  Serial.print("\t");
-  Serial.println(ens210.getHumidityPercent())
+  if (ens210.available()) {
+    ens210.measure();
+    Serial.print("ENS210:");
+    Serial.print("\t");
+    Serial.print(ens210.getTempCelsius());
+    Serial.print("°C\t");
+    Serial.print(ens210.getHumidityPercent());
+    Serial.print("%\t");
+  }
+  
+  Serial.println();
   
   delay(1000); 
 }
